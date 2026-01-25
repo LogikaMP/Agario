a92b969c6b174615b35bc55c6af16ba716c79300
LogikaMP <logika690@gmail.com>
2026-01-25 16:25:29 +0200

    1 part agario

--- agario.py
+++ agario.py
@@ -1,14 +1,14 @@
 # Імпортуємо бібліотеку для створення ігор
-import
+import pygame
 
 # Імпортуємо функцію для генерації випадкових чисел
-from 
+from random import randint
 
 # Імпортуємо всі класи та функції з файлу my_class.py
-from 
+from my_class import * 
 
 # Ініціалізуємо всі модулі Pygame
-pygame.
+pygame.init()
 
 # Задаємо ширину і висоту вікна гри
 WIDTH, HEIGHT = 500, 500
@@ -21,38 +21,59 @@ GREEN = (0, 200, 0)
 
 
 # Створюємо вікно гри з заданими розмірами
-window = 
+window = pygame.display.set_mode((WIDTH, HEIGHT))
 # Створюємо об'єкт для контролю частоти кадрів (FPS)
-clock = 
+clock = pygame.time.Clock() 
+
 
 # Змінна для збереження зображення фону (поки що порожня)
-fon = 
+fon = pygame.image.load("fon.png")
+fon = pygame.transform.scale(fon,(WIDTH,HEIGHT))
 
-
+#гавець
+player = Player(x = WIDTH//2,y = HEIGHT//2, radius=20,speed = 5,color = GREEN)
+#їжф
+foods = [Food() for i in range(300)]
 # Змінна для керування роботою гри
-run = 
+run = True
 
-
+fon_x, fon_y = 0,0
 # Головний цикл гри, працює поки run == True
-while 
+while run:
+    window.fill(BLACK)
+    window.blit(fon,(fon_x,fon_y))
+    window.blit(fon,(fon_x-WIDTH,fon_y))
+    window.blit(fon,(fon_x,fon_y - HEIGHT))
+    window.blit(fon,(fon_x - WIDTH,fon_y - HEIGHT))
+    player.draw(window)
+    player.move()
+    fon_x += player.move_x
+    fon_y += player.move_y
+    fon_x %= WIDTH
+    fon_y %= HEIGHT
+# промальовкаб, оновлення координат відносно гравця, перевірка зіткнення з гравцем
 
-    # Зафарбовуємо екран чорним кольором
-    window.
+    for food in foods:
+        food.draw(window)
+        food.update(player)
+        if food.eat_me(player):
+            foods.remove(food)
 
-    # Малюємо фон у верхньому лівому куті (помилка, якщо fon == None)
-    window.
+    for event in pygame.event.get():
 
-    # Перебираємо всі події (натискання клавіш, закриття вікна тощо)
-    for 
 
         # Перевіряємо, чи користувач закрив вікно
-        if 
+        if event.type == pygame.QUIT:
+
 
             # Зупиняємо гру
-            run 
+            run = False
 
     # Оновлюємо зображення на екрані
-    pygame.
+    pygame.display.flip()
 
     # Обмежуємо швидкість гри до 60 кадрів на секунду
-    clock.
+    clock.tick(60)
+
+
+
